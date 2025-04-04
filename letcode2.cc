9b35526b9aa601b3820f636694ee4b2598012f4c
Nena-anghan <anghannena99@gmail.com>
2025-04-04 11:27:05 +0530

    Update letcode2.cc

--- letcode2.cc
+++ letcode2.cc
@@ -1,26 +1,26 @@
-// class Solution {
-//     public:
-//         int longestConsecutive(vector<int>& nums)   
-//         {
-//             if (nums.empty()) {
-//             return 0;
-//         }
+class Solution {
+    public:
+        int longestConsecutive(vector<int>& nums)   
+        {
+            if (nums.empty()) {
+            return 0;
+        }
     
-//         sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end());
     
-//         int count = 1, max_count = 1; 
-//         for (int i = 1; i < nums.size(); i++) {
-//             if (nums[i] == nums[i - 1]) {
-//                 continue; 
-//             }
-//             if (nums[i] == nums[i - 1] + 1) {
-//                 count++;  
-//             } else {
-//                 max_count = max(max_count, count);
-//                 count = 1; 
-//             }
-//         }
-//         max_count = max(max_count, count);
-//           return max_count; 
-//         }
-//     };
+        int count = 1, max_count = 1; 
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1]) {
+                continue; 
+            }
+            if (nums[i] == nums[i - 1] + 1) {
+                count++;  
+            } else {
+                max_count = max(max_count, count);
+                count = 1; 
+            }
+        }
+        max_count = max(max_count, count);
+          return max_count; 
+        }
+    };
